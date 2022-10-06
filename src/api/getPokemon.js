0b6b446184a02e1ad369d616a92e6fa2609be9b1
Mukul Chugh <mukulchugh@Mukuls-MacBook-Air.local>
2022-10-06 22:40:52 +0530

    Update: Fixed the API issue

--- src/api/getPokemon.js
+++ src/api/getPokemon.js
@@ -1,9 +1,8 @@
-const baseUrl = 'http://pokeapi.co/api/v2';
+const baseUrl = 'https://pokeapi.co/api/v2';
 const query = {
   pokemon: 'pokemon'
 }
 
 export async function fetchPokemon(pokemon) {
-  console.log(`${baseUrl}/${query.pokemon}/${pokemon}`);
   return fetch(`${baseUrl}/${query.pokemon}/${pokemon}`)
 }
